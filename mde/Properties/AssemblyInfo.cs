a01161fcec7fdb4974bccef923c28208874a0eda
tanno-seiichi <tantans.yhc@gmail.com>
2026-08-23 02:00:03 +0900

    v2.0.4

--- mde/Properties/AssemblyInfo.cs
+++ mde/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //      ビルド番号
 //      リビジョン
 //
-[assembly: AssemblyVersion( "2.0.3.0" )]
-[assembly: AssemblyFileVersion( "2.0.3.0" )]
+[assembly: AssemblyVersion( "2.0.4.0" )]
+[assembly: AssemblyFileVersion( "2.0.4.0" )]
